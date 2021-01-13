eeb3bed0637e9b39ec50fe7caf6f294ef1db6950
willxj36 <willxj36@gmail.com>
2021-01-12 20:39:06 -0700

    fixed timeouts, working on promises section

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ log = message => console.log(message);
 log('Hallo wie geht es dir');
 setTimeout(() => log('Es geht sehr gut'), 2000);
 
-async function getWords() {
-    await Promise.resolve(console.log('first'));
-    await Promise.resolve(setTimeout(() => console.log('second'), 3000));
-    await Promise.resolve(setTimeout(() => console.log('third'), 2000));
-    Promise.resolve(setTimeout(() => console.log('fourth'), 1000));
+function getWords() {
+    console.log('first');
+    setTimeout(() => {
+        console.log('second');
+        setTimeout(() => {
+            console.log('third');
+            setTimeout(() => {
+                console.log('fourth');
+            }, 1000)
+        }, 2000)
+    }, 3000)
 }
 
-getWords(); //all settimeouts are being fired at same time, need to figure out why
+getWords();
 
+function done() {console.log("Job's done!")};
+
+countdown = (num, callback) => {
+    setTimeout(() => {
+        if(num > 0) {
+            console.log(num);
+            countdown(num - 1, callback);
+        } else {
+            callback();
+        }
+    }, 1000);
+};
+
+countdown(10, done);
+
+let lunchTime = true;
+const orderMeSomeFood = () => {
+    return new Promise((resolve, reject) => {
+        //stuff
+    }
